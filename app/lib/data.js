6b9945af9285804fa3c02fb027da5877cfcfbdaa
Hildacaro <hildacarodcr@gmail.com>
2024-03-20 00:29:32 +0100

    feat(): change URL to API

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,35 +1,27 @@
 // Primero, instala axios con npm install axios
 import axios from 'axios';
 
+// base de la API
+const apiUrl = 'https://hilda.v2.proyectosdwa.es/api/public/api';
+
 // data.js
 export async function getWeatherData() {
-  console.log("🚀 ~ getWeatherData ~ process:", process)
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=madrid&lang=es&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`;
   const response = await axios.get(URL);
-  console.log("🚀 ~ getWeatherData ~ response:", response)
   return response.data;
 }
 
 export async function fetchNews() {
-  const URL = `http://localhost:8000/api/news`;
-  const response = await axios.get(URL);
-  console.log("🚀 ~ fetchNews ~ response:", response)
-  console.log("🚀 ~ fetchNews ~ URL:", URL)
+  const response = await axios.get(apiUrl + '/news');
   return response.data;
 }
 
 export async function fetchFAQs() {
-  const URL = `http://localhost:8000/api/faqs`;
-  const response = await axios.get(URL);
-  console.log("🚀 ~ fetchNews ~ response:", response)
-  console.log("🚀 ~ fetchNews ~ URL:", URL)
+  const response = await axios.get(apiUrl + '/faqs');
   return response.data;
 }
 
 export async function fetchTeam() {
-  const URL = `http://localhost:8000/api/team`;
-  const response = await axios.get(URL);
-  console.log("🚀 ~ fetchTeam ~ response:", response)
-  console.log("🚀 ~ fetchTeam ~ URL:", URL)
+  const response = await axios.get(apiUrl + '/team');
   return response.data;
 }
